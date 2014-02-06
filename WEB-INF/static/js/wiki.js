fb48b6b0bd347c7e6ab979505dc7523d6758711e
yuinacor <yuiraven@naver.com>
2014-02-06 16:59:27 +0900

    initial files
    test html
    index.html
    test js
    wiki.js
    lib
    jquery 2.1.0

---
+++ WEB-INF/static/js/wiki.js
@@ -0,0 +1,105 @@
+Array.prototype.unique = function() {
+	var a = {};
+	for ( var i = 0; i < this.length; i++) {
+		if (typeof a[this[i]] == "undefined")
+			a[this[i]] = 1;
+	}
+	this.length = 0;
+	for ( var i in a)
+		this[this.length] = i;
+	return this;
+};
+
+var Wiki = function() {
+};
+/**
+ * 위키 마크업으로 작성된 문서를 파싱하는 객체
+ */
+Wiki.Parser = new (function() {
+	var domain = location.pathname;
+	var regexSets = [ {
+		// h1 parser ======text======
+		type : "h1",
+		regex : /\={6}(.*?)\={6}/g,
+		replaceStatement : "<h1>$1</h1>"
+	}, {
+		// h2 parser =====text=====
+		type : "h2",
+		regex : /\={5}(.*?)\={5}/g,
+		replaceStatement : "<h2>$1</h2>"
+	}, {
+		// h3 parser ====text====
+		type : "h3",
+		regex : /\={4}(.*?)\={4}/g,
+		replaceStatement : "<h3>$1</h3>"
+	}, {
+		// h4 parser ===text===
+		type : "h4",
+		regex : /\={3}(.*?)\={3}/g,
+		replaceStatement : "<h4>$1</h4>"
+	}, {
+		// h5 parser ==text==
+		type : "h5",
+		regex : /\={2}(.*?)\={2}/g,
+		replaceStatement : "<h5>$1</h5>"
+	}, {
+		// strong parser **text**
+		type : "bold",
+		regex : /\*{2}(.*?)\*{2}/g,
+		replaceStatement : "<strong>$1</strong>"
+	}, {
+		// link parser [[text]]
+		type : "link",
+		regex : /\[\[(.*?)\]\]/g,
+		replaceStatement : "<a href='" + domain + "?id=$1'>$1</a>"
+	} ];
+
+	/**
+	 * 헤드라인별로 div로 묶는다.
+	 * 
+	 * @author YuiNacor
+	 * @param {string} 파싱을 수행할 본문 문서
+	 * @returns {string} div로 묶은 후의 문서
+	 * @version 1.0
+	 */
+	var makeDomHierarchy = function(string) {
+		var regex = /(\={1,6}.*?\={1,6})/g;
+		var matchArr = string.match(regex);
+		
+		//matchArr에서 중복요소를 제거해야 한다.
+		matchArr.unique();
+		string = string.replace(matchArr[0], "<div>" + matchArr[0]);
+
+		for ( var i = 1, max = matchArr.length; i < max; i++) {
+			string = string.replace(matchArr[i], "</div><div>" + matchArr[i]);
+		}
+		return string;
+	};
+
+	/**
+	 * 문서 전체의 파싱을 수행
+	 * 
+	 * @author YuiNaCor
+	 * @param {string} 파싱을 수행할 본문 문서
+	 * @returns {element} 파싱 후 하나의 div로 랩핑된 문서
+	 * @version 1.0
+	 */
+	this.replaceAll = function(string) {
+
+		string = makeDomHierarchy(string);
+
+		for ( var i = 0; i < regexSets.length; i++) {
+			string = string.replace(regexSets[i].regex,
+					regexSets[i].replaceStatement);
+		}
+		return "<div>" + string + "</div>";
+	};
+
+});
+
+document.getElementById("confirm").addEventListener("click", function(e) {
+	var dom = document.getElementById("wikiText");
+
+	document.write(Wiki.Parser.replaceAll(dom.value));
+	// document.write("<div>" + Wiki.parser.replaceAll(dom.value) + "</div>");
+});
